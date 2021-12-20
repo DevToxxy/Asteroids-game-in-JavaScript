4aac45860502d4d297be9b383605fc30b171a1a6
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2021-12-20 18:04:55 +0100

    Cleanup

--- asteroid.js
+++
@@ -1,7 +0,0 @@
-export default class Asteroid extends Phaser.Physics.Arcade.Sprite {
-
-    constructor(scene, x, y, sprite){
-      super(scene,x,y,sprite);
-      this.setVelocity(150,150);
-    }
-  }
